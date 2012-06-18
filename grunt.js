39a087947de78acf7537d0c0e2d3578dce37a095
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-06-18 16:48:58 +0200

    Upgrade to grunt-jquery-content

--- grunt.js
+++ grunt.js
@@ -1,24 +1,11 @@
 /*jshint node:true */
 module.exports = function( grunt ) {
 
-var // modules
-	fs = require( "fs" ),
-	path = require( "path" ),
-	pygmentize = require( "pygmentize" ),
-	spawn = require( "child_process" ).spawn,
-
-	// files
-	entryFiles = grunt.file.expandFiles( "entries/*.xml" ),
-	resourceFiles = grunt.file.expandFiles( "resources/*" ),
-
-	xmlFiles = [].concat( entryFiles, "cat2tax.xsl", "categories.xml", "entries2html.xsl", "xml2json.xsl" );
-
-function pathSlug( fileName ) {
-	return path.basename( fileName, path.extname( fileName ) );
-}
+var entryFiles = grunt.file.expandFiles( "entries/*.xml" );
 
 grunt.loadNpmTasks( "grunt-clean" );
 grunt.loadNpmTasks( "grunt-wordpress" );
+grunt.loadNpmTasks( "grunt-jquery-content" );
 
 grunt.initConfig({
 	clean: {
@@ -28,150 +15,24 @@ grunt.initConfig({
 		grunt: "grunt.js"
 	},
 	xmllint: {
-		all: xmlFiles
+		all: [].concat( entryFiles, "cat2tax.xsl", "categories.xml", "entries2html.xsl", "xml2json.xsl" )
+	},
+	xmltidy: {
+		all: [].concat( entryFiles, "categories.xml" )
+	},
+	"build-xml-entries": {
+		all: entryFiles
+	},
+	"build-resources": {
+		all: grunt.file.expandFiles( "resources/*" )
 	},
 	wordpress: grunt.utils._.extend({
 		dir: "dist/wordpress"
 	}, grunt.file.readJSON( "config.json" ) )
 });
 
-grunt.registerTask( "xmllint", function() {
-	var task = this,
-		taskDone = task.async();
-	grunt.utils.async.forEachSeries( xmlFiles, function( fileName, fileDone ) {
-		grunt.verbose.write( "Linting " + fileName + "..." );
-		grunt.utils.spawn({
-			cmd: "xmllint",
-			args: [ "--noout", fileName ]
-		}, function( err, result ) {
-			if ( err ) {
-				grunt.verbose.error();
-				grunt.log.error( err );
-				fileDone();
-				return;
-			}
-			grunt.verbose.ok();
-			fileDone();
-		});
-	}, function() {
-		if ( task.errorCount ) {
-			grunt.warn( "Task \"" + task.name + "\" failed." );
-			taskDone();
-			return;
-		}
-		grunt.log.writeln( "Lint free files: " + entryFiles.length );
-		taskDone();
-	});
-});
-
-grunt.registerTask( "build-entries", function() {
-	var task = this,
-		taskDone = task.async(),
-		// TODO make `entry` a custom post type instead of (ab)using `post`?
-		targetDir = grunt.config( "wordpress.dir" ) + "/posts/post/";
-
-	grunt.file.mkdir( targetDir );
-
-	grunt.utils.async.forEachSeries( entryFiles, function( fileName, fileDone ) {
-		grunt.verbose.write( "Reading " + fileName + "..." );
-		grunt.utils.spawn({
-			cmd: "xsltproc",
-			args: [ "entries2html.xsl", fileName ]
-		}, function( err, result ) {
-			var targetFileName;
-			if ( err ) {
-				grunt.verbose.error();
-				grunt.log.error( err );
-				fileDone();
-				return;
-			}
-			grunt.verbose.ok();
-
-			targetFileName = targetDir + path.basename( fileName );
-			targetFileName = targetFileName.substr( 0, targetFileName.length - "xml".length ) + "html";
-
-			grunt.verbose.write( "Pygmentizing " + targetFileName + "..." );
-			pygmentize.file( result, function( error, data ) {
-				if ( error ) {
-					grunt.verbose.error();
-					grunt.log.error( error );
-					fileDone();
-					return;
-				}
-				grunt.verbose.ok();
-
-				grunt.file.write( targetFileName, data );
-
-				fileDone();
-			});
-
-		});
-	}, function() {
-		if ( task.errorCount ) {
-			grunt.warn( "Task \"" + task.name + "\" failed." );
-			taskDone();
-			return;
-		}
-		grunt.log.writeln( "Built " + entryFiles.length + " entries." );
-		taskDone();
-	});
-});
-
-grunt.registerTask( "build-categories", function() {
-	var task = this,
-		taskDone = task.async(),
-		categories = {},
-		outFilename = grunt.config( "wordpress.dir" ) + "/taxonomies.json";
-
-		grunt.utils.spawn({
-			cmd: "xsltproc",
-			args: [ "--output", "taxonomies.xml", "cat2tax.xsl", "categories.xml" ]
-		}, function( err, result ) {
-			if ( err ) {
-				grunt.verbose.error();
-				grunt.log.error( err );
-				taskDone();
-				return;
-			}
-			grunt.utils.spawn({
-				cmd: "xsltproc",
-				args: [ "--output", outFilename, "xml2json.xsl", "taxonomies.xml" ]
-			}, function( err, result ) {
-			if ( err ) {
-				grunt.verbose.error();
-				grunt.log.error( err );
-				taskDone();
-				return;
-			}
-			fs.unlinkSync( "taxonomies.xml" );
-			grunt.verbose.ok();
-			taskDone();
-		});
-	});
-});
-
-grunt.registerTask( "build-resources", function() {
-	var task = this,
-		taskDone = task.async(),
-		targetDir = grunt.config( "wordpress.dir" ) + "/resources/";
-
-	grunt.file.mkdir( targetDir );
-
-	grunt.utils.async.forEachSeries( resourceFiles, function( fileName, fileDone )  {
-		grunt.file.copy( fileName, targetDir + path.basename( fileName ) );
-		fileDone();
-	}, function() {
-		if ( task.errorCount ) {
-			grunt.warn( "Task \"" + task.name + "\" failed." );
-			taskDone();
-			return;
-		}
-		grunt.log.writeln( "Built " + resourceFiles.length + " resources." );
-		taskDone();
-	});
-});
-
 grunt.registerTask( "default", "build-wordpress" );
-grunt.registerTask( "build-wordpress", "clean lint xmllint build-entries build-categories build-resources" );
+grunt.registerTask( "build-wordpress", "clean lint xmllint build-xml-entries build-xml-categories build-resources" );
+grunt.registerTask( "tidy", "xmllint xmltidy" );
 
 };
