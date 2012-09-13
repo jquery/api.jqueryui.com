0ccfc1904c408662eb4c6df403c6037f0e7c7450
Scott González <scott.gonzalez@gmail.com>
2012-09-13 12:47:33 -0400

    Upgraded grunt-jquery-content to 0.5.x.

--- grunt.js
+++ grunt.js
@@ -13,13 +13,8 @@ grunt.initConfig({
 	lint: {
 		grunt: "grunt.js"
 	},
-	// TODO would be useful, but throws broken exceptions when a change is detected
-	watch: {
-		files: "entries/**",
-		tasks: "grunt deploy"
-	},
 	xmllint: {
-		all: [].concat( entryFiles, "cat2tax.xsl", "categories.xml", "entries2html.xsl", "xml2json.xsl" )
+		all: [].concat( entryFiles, "categories.xml", "entries2html.xsl", "notes.xsl" )
 	},
 	xmltidy: {
 		all: [].concat( entryFiles, "categories.xml" )
@@ -39,7 +34,8 @@ grunt.initConfig({
 });
 
 grunt.registerTask( "default", "build-wordpress" );
-grunt.registerTask( "build-wordpress", "clean lint xmllint build-pages build-xml-entries build-xml-categories build-resources" );
+grunt.registerTask( "build", "build-pages build-xml-entries build-xml-categories build-resources build-xml-full" );
+grunt.registerTask( "build-wordpress", "clean lint xmllint build" );
 grunt.registerTask( "tidy", "xmllint xmltidy" );
 
 };
