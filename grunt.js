4f862a231dd3c8315041d06814e0b67fe48c7949
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-07-01 18:19:00 -0300

    Manifest: Generate categories.json manifest
    - Get categories metadata and extend it with its posts by using
      wordpress-walk-posts

--- grunt.js
+++ grunt.js
@@ -34,6 +34,35 @@ grunt.initConfig({
 	}, grunt.file.readJSON( "config.json" ) )
 });
 
+grunt.registerTask( "manifest", "Generate categories.json manifest file", function() {
+	var categories,
+		categoryPosts = {},
+		done = this.async();
+
+	categories = require( "./" + grunt.config( "wordpress.dir" ) + "/taxonomies" ).category;
+	categories.forEach(function( category ) {
+		category.posts = categoryPosts[ category.slug ] = [];
+	});
+
+	grunt.helper( "wordpress-walk-posts", grunt.config( "wordpress.dir" ), function( post, callback ) {
+		if ( post.termSlugs && post.termSlugs.category ) {
+			post.termSlugs.category.forEach(function( slug ) {
+				categoryPosts[ slug ].push( post );
+			});
+		}
+		callback();
+	}, function( error ) {
+		if ( error ) {
+			grunt.error.log( error.message );
+			return done( false );
+		}
+
+		grunt.file.write( grunt.config( "wordpress.dir" ) + "/categories.json",
+			JSON.stringify( categories, null, "\t" ) + "\n" );
+		done();
+	});
+});
+
 grunt.registerTask( "default", "build-wordpress" );
 grunt.registerTask( "build", "build-pages build-xml-entries build-xml-categories build-resources build-xml-full" );
 grunt.registerTask( "build-wordpress", "check-modules clean lint xmllint build" );
