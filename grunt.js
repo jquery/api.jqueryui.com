ad33f105bc9ea456af950c06268ab195b4dda72b
Scott González <scott.gonzalez@gmail.com>
2014-12-02 17:50:50 -0500

    Build: Remove unused tasks (cherry picked from commit f4db51e3b03cfb22ead2c25b367b4cbda63821ba)
    Conflicts:
    
    	grunt.js

--- grunt.js
+++ grunt.js
@@ -4,19 +4,13 @@ module.exports = function( grunt ) {
 
 var entryFiles = grunt.file.expandFiles( "entries/*.xml" );
 
-grunt.loadNpmTasks( "grunt-wordpress" );
-grunt.loadNpmTasks( "grunt-jquery-content" );
 grunt.loadNpmTasks( "grunt-check-modules" );
+grunt.loadNpmTasks( "grunt-jquery-content" );
+grunt.loadNpmTasks( "grunt-wordpress" );
 
 grunt.initConfig({
-	lint: {
-		grunt: "grunt.js"
-	},
 	xmllint: {
 		all: [].concat( entryFiles, "categories.xml", "entries2html.xsl", "notes.xsl" )
-	},
-	xmltidy: {
-		all: [].concat( entryFiles, "categories.xml" )
 	},
 	"build-pages": {
 		all: grunt.file.expandFiles( "pages/**" )
@@ -65,9 +59,7 @@ grunt.registerTask( "manifest", "Generate categories.json manifest file", functi
 	});
 });
 
-grunt.registerTask( "default", "build-wordpress" );
 grunt.registerTask( "build", "build-pages build-xml-entries build-xml-categories build-resources build-xml-full" );
-grunt.registerTask( "build-wordpress", "check-modules clean lint xmllint build" );
-grunt.registerTask( "tidy", "xmllint xmltidy" );
+grunt.registerTask( "build-wordpress", "check-modules clean xmllint build" );
 
 };
